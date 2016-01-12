a4a573a349d32ed5867747b4c5ff327ec372883e
galenjiang <249254832@qq.com>
2016-01-12 23:28:44 +0800

    update

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
-var express = require('express');
-var app = express();
+// express_cookie.js 文件
+var express      = require('express')
+var cookieParser = require('cookie-parser')
 
-app.use(express.static('public'));
+var app = express()
+app.use(cookieParser())
 
-app.get('/index.htm', function(req, res) {
-
-  res.sendFile(__dirname + "/" + "index.htm");
+app.get('/', function(req, res) {
+  console.log("Cookies: ", req.cookies)
 })
 
-app.get('/process_get', function(req, res) {
-
-  // 输出 JSON 格式
-  response = {
-    first_name: req.query.first_name,
-    last_name: req.query.last_name
-  };
-  console.log(response);
-  res.send(JSON.stringify(response));
-})
-
-var server = app.listen(8081, function() {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
-
-})
+app.listen(8081)
