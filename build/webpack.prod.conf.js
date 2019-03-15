ba02679158fe632109873da86298760f69fdc386
galen <249254832@qq.com>
2019-03-15 11:34:25 +0800

    :arrow_up: update all packages

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -6,26 +6,31 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = merge(baseConfig, {
-    mode: 'production',
-    devtool: "source-map",
-    module: {
-        rules: [],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name]_[chunkhash:4].css"
-        }),
+  mode: 'production',
+  devtool: "source-map",
+  module: {
+    rules: [],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name]_[chunkhash:4].css"
+    }),
 
-        new HtmlWebpackPlugin({
-            template: 'public/index.ejs',
-            templateParameters: {
-                'NODE_ENV': process.env.NODE_ENV
-            },
-        }),
+    new HtmlWebpackPlugin({
+      template: 'public/index.ejs',
+      templateParameters: {
+        'NODE_ENV': process.env.NODE_ENV
+      },
+    }),
 
-        new CleanWebpackPlugin(['dist'], {
-            root: path.resolve(__dirname, '..'),
-            verbose: true
-        })
-    ],
+    new CleanWebpackPlugin({
+      root: path.resolve(__dirname, '..'),
+      verbose: true
+    }),
+
+    new CleanWebpackPlugin(['dist'], {
+      root: path.resolve(__dirname, '..'),
+      verbose: true
+    })
+  ],
 })
